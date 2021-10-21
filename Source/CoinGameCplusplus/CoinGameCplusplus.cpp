4c487d4d14fac8e22a33a92e3e74e356e8f53831
aaaa <mai_19n1102996@nnn.ed.jp>
2021-10-21 14:50:48 +0900

    削除

--- Source/CoinGameCplusplus/CoinGameCplusplus.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CoinGameCplusplus.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CoinGameCplusplus, "CoinGameCplusplus" );
- 
